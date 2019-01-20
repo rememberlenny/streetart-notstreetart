ad2b47a2514700cf5aca089b7f0dde2537aee91b
Leonard Bogdonoff <rememberlenny@gmail.com>
2019-01-19 23:49:10 -0500

    Save build dataset file

---
+++ build_dataset.py
@@ -0,0 +1,62 @@
+# USAGE
+# python build_dataset.py
+
+# import the necessary packages
+from pyimagesearch import config
+from imutils import paths
+import random
+import shutil
+import os
+
+# grab the paths to all input images in the original input directory
+# and shuffle them
+imagePaths = list(paths.list_images(config.ORIG_INPUT_DATASET))
+random.seed(42)
+random.shuffle(imagePaths)
+
+# compute the training and testing split
+i = int(len(imagePaths) * config.TRAIN_SPLIT)
+trainPaths = imagePaths[:i]
+testPaths = imagePaths[i:]
+
+# we'll be using part of the training data for validation
+i = int(len(trainPaths) * config.VAL_SPLIT)
+valPaths = trainPaths[:i]
+trainPaths = trainPaths[i:]
+
+# define the datasets that we'll be building
+datasets = [
+	("training", trainPaths, config.TRAIN_PATH),
+	("validation", valPaths, config.VAL_PATH),
+	("testing", testPaths, config.TEST_PATH)
+]
+
+# loop over the datasets
+for (dType, imagePaths, baseOutput) in datasets:
+	# show which data split we are creating
+	print("[INFO] building '{}' split".format(dType))
+
+	# if the output base output directory does not exist, create it
+	if not os.path.exists(baseOutput):
+		print("[INFO] 'creating {}' directory".format(baseOutput))
+		os.makedirs(baseOutput)
+
+	# loop over the input image paths
+	for inputPath in imagePaths:
+		# extract the filename of the input image along with its
+		# corresponding class label
+		filename = inputPath.split(os.path.sep)[-1]
+		label = inputPath.split(os.path.sep)[-2]
+
+		# build the path to the label directory
+		labelPath = os.path.sep.join([baseOutput, label])
+
+		# if the label output directory does not exist, create it
+		if not os.path.exists(labelPath):
+			print("[INFO] 'creating {}' directory".format(labelPath))
+			os.makedirs(labelPath)
+
+		# construct the path to the destination image and then copy
+		# the image itself
+		p = os.path.sep.join([labelPath, filename])
+		shutil.copy2(inputPath, p)
